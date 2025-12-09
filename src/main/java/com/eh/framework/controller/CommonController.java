cfcc509b748d7cb1bcbaede8ce8635e2072ed92a
seh <hyeegood@gmail.com>
2025-12-09 13:16:22 +0900

    extends commonController

---
+++ src/main/java/com/eh/framework/controller/CommonController.java
@@ -0,0 +1,5 @@
+package com.eh.framework.controller;
+
+public class CommonController {
+
+}
--- src/main/java/com/eh/framework/controller/HomeController.java
+++ src/main/java/com/eh/framework/controller/HomeController.java
@@ -4,9 +4,11 @@ package com.eh.framework.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+@RequestMapping("/home")
+public class HomeController extends CommonController {
 	
 	@GetMapping("/home")
     public String home(Model model) {
